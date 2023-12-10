791f532bb144e90f01163d30cf219e0910ba5f63
Shilpii01 <shilpicharu@gmail.com>
2023-12-10 22:53:19 +1300

    Initial Commit

---
+++ Mars_CompetitionTask/Mars_CompetitionTask/Utilities/CommonDriver.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace Mars_CompetitionTask.Utilities
+{
+    [TestFixture]
+    [Binding]
+   
+    public class CommonDriver
+    {
+        
+        public static IWebDriver driver;
+
+        
+        public void Initialize()
+        {
+            //Defining the browser
+            driver = new ChromeDriver();
+
+            //Maximize the window
+            driver.Manage().Window.Maximize();
+        }
+        
+        public void Close()
+        {
+            
+                driver.Close();
+                            
+        }
+
+    }
+}
---
+++ Mars_CompetitionTask/Mars_CompetitionTask/Utilities/Wait.cs
@@ -0,0 +1,50 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+
+namespace Mars_CompetitionTask.Utilities
+{
+    public class Wait
+    {
+        public static void WaitToBeClickable(IWebDriver driver, string locatorType, string locatorValue, int seconds)
+        {
+            var wait = new WebDriverWait(driver, new TimeSpan(0, 0, seconds));
+            if (locatorType == "Xpath")
+            {
+                wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementToBeClickable(By.XPath(locatorValue)));
+            }
+            if (locatorType == "Id")
+            {
+                wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementToBeClickable(By.Id(locatorValue)));
+            }
+            if (locatorType == "CssSelector")
+            {
+                wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementToBeClickable(By.CssSelector(locatorValue)));
+            }
+            if (locatorType == "Name")
+            {
+                wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementToBeClickable(By.Name(locatorValue)));
+            }
+        }
+        public static void WaitToBeVisible(IWebDriver driver, string locatorType, string locatorValue, int seconds)
+        {
+            var wait = new WebDriverWait(driver, new TimeSpan(0, 0, seconds));
+            if (locatorType == "Xpath")
+            {
+                wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementIsVisible(By.XPath(locatorValue)));
+            }
+            if (locatorType == "Id")
+            {
+                wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementIsVisible(By.Id(locatorValue)));
+            }
+            if (locatorType == "CssSelector")
+            {
+                wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementToBeClickable(By.CssSelector(locatorValue)));
+            }
+            if (locatorType == "Name")
+            {
+                wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementToBeClickable(By.Name(locatorValue)));
+            }
+        }
+
+    }
+}
